15430db26b41d03d5f4e69c0436610421d0b3a36
Squiddo <squiddytoonart@gmail.com>
2026-08-10 12:57:43 -0600

    shots fire with delay and recharge time
    The standard ray also now shows white when colliding and gray when not

--- SpiteTowerGameFiles/tower-of-spite/LaserEye.cs
+++ SpiteTowerGameFiles/tower-of-spite/LaserEye.cs
@@ -9,13 +9,18 @@ public partial class LaserEye : Node2D
 	
 	[Export] public float DefaultShotTimer = 3f; // in seconds; The default time the Laser Eye waits before shooting at the player
 	private float ShotTimer = 3f; // in seconds;
-	[Export] public float ShotDelay = 0.1f; // in seconds; When ShotTimer is below this number, it will stop tracking the player to prepare to shoot.
+	[Export] public float DefaultShotDelay = 0.5f; // in seconds; The time it takes to wait before shooting at the player
+	private float ShotDelay = 0.1f; // in seconds;
+	[Export] public float DefaultRecharge = 0.3f;
+	private float RechargeTimer = 0.3f;
 	
 	private RayCast2D RayCast;
 	private Timer Timer;
 	private float CastAngle = 0;
 	
 	private bool IsTracking = true;
+	private bool IsCharging = false;
+	private bool IsShooting = false;
 
 	private Line2D DebugLine;
 
@@ -23,10 +28,8 @@ public partial class LaserEye : Node2D
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		_rayCast = GetNode<RayCast2D>("RayCast2D");
-		_rayCast.SetTargetPosition(Vector2.Down * RayMaxLength);
 		RayCast = GetNode<RayCast2D>("RayCast2D");
-		RayCast.SetTargetPosition(Vector2.Down * RayLength);
+		RayCast.SetTargetPosition(Vector2.Down * RayMaxLength);
 		
 		DebugLine = GetNode<Line2D>("DebugLine");
 		DebugLine.Show();
@@ -36,26 +39,46 @@ public partial class LaserEye : Node2D
 	public override void _Process(double delta)
 	{
 		
-		// Making sure the target even exists
-		if (Target != null)
 		// Null Target Protection
 		if (Target == null)
 		{
 			RayCast.TargetPosition = Vector2.Down * RayLength;
 			IsTracking = false;
 		}
-		
-		// Makes the debug line have the same points as the raycast for visibility's sake
-		_debugLine.Points = new Vector2[2] {_rayCast.Position, _rayCast.GetTargetPosition() - Position};
-		
-		// TODO: Make main raycast go till it hits ground/wall
-		// TODO: Make a Shoot function that stops tracking the player and.. yknow, *shoots*
 		else
 		{
 			IsTracking = true;
 		}
+		
+		// TODO: Make main raycast go till it hits ground/wall
 
-		// Shooting
+		if (IsTracking && !IsCharging)
+		{
+			_aim();
+			_debugUpdate();
+
+			ShotTimer -= (float)delta;
+			if (ShotTimer <= 0)
+			{
+				IsCharging = true;
+			}
+		}
+		else if (IsCharging)
+		{
+			if (!IsShooting)
+			{
+				IsShooting = true;
+				ShotDelay = DefaultShotDelay;
+				RechargeTimer = DefaultRecharge;
+			}
+			Shoot(delta);
+		}
+		else
+		{
+			ShotTimer = DefaultShotTimer;
+		}
+		
+		// Old nonfunctional Shooting
 		/*
 		if (IsTracking && ShotTimer >= ShotDelay)
 		{
@@ -76,35 +99,55 @@ public partial class LaserEye : Node2D
 			IsTracking = true;
 			ShotTimer = DefaultShotTimer;
 		*/
+	}
+
+	public void Shoot(double delta)
+	{
+		DebugLine.DefaultColor = Colors.Orange;
 		
-		// Tracking Direction Calculation
-		if (IsTracking)
+		if (ShotDelay >= 0)
 		{
-			// _rayCast.TargetPosition = Vector2.FromAngle(_castAngle) * RayLength;
-			// _castAngle = _rayCast.TargetPosition.AngleTo(Target.Position - _rayCast.GlobalPosition);
-			RayCast.TargetPosition = RayCast.TargetPosition.DirectionTo((Target.Position - RayCast.GlobalPosition) * RayLength);
-			RayCast.TargetPosition *= RayLength;
-			
-			// _rayCast.TargetPosition = Vector2.FromAngle((Target.Position - _rayCast.TargetPosition ).Angle()) * RayLength;
+			ShotDelay -= (float)delta;
+		}
+		else
+		{
+			// TODO: Add hitbox for the shot
+			DebugLine.DefaultColor = Colors.LimeGreen;
+		
+			if (RechargeTimer >= 0)
+			{
+				RechargeTimer -= (float)delta;
+			}
+			else
+			{
+				
+				IsCharging = false;
+				ShotTimer = DefaultShotTimer;
+				IsShooting = false;
+			}
 		}
 		
-		// Debug Line
+		GD.Print(ShotDelay);
+		GD.Print(RechargeTimer);
+	}
+
+	private void _aim()
+	{
+		RayCast.TargetPosition = RayCast.TargetPosition.DirectionTo((Target.Position - RayCast.GlobalPosition) * RayLength);
+		RayCast.TargetPosition *= RayLength;
+	}
+
+	private void _debugUpdate()
+	{
 		DebugLine.Points = new Vector2[2] {RayCast.Position, RayCast.GetTargetPosition()};
 		
 		if (RayCast.IsColliding())
 		{
-			DebugLine.SetDefaultColor(Colors.Crimson);
+			DebugLine.SetDefaultColor(Colors.White);
 		}
 		else
 		{
-			DebugLine.SetDefaultColor(Colors.White);
+			DebugLine.SetDefaultColor(Colors.Gray);
 		}
-	}
-
-	public void Shoot()
-	{
-		// TODO: Actually shoot the damn thing
-		IsTracking = true;
-		ShotTimer = DefaultShotTimer;
 	}
 }
