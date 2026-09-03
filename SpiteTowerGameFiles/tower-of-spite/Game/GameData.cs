14259feff5452dfad97cb3ebf524c99c6a235105
24BobbyJoe <24bobyjoe@gmail.com>
2026-09-03 10:03:36 -0600

    fixed error

--- SpiteTowerGameFiles/tower-of-spite/Game/GameData.cs
+++ SpiteTowerGameFiles/tower-of-spite/Game/GameData.cs
@@ -15,7 +15,7 @@ public static class GameData
 	public static int FloorNumber = 1;
 	public static bool IsOpen { get; set; } = false;
 	public static List<PackedScene> Hazards { get; set; } = [];
-	public static List<string[]> Abilities { get; set; } = [];
+	public static List<string[]> Abilities { get; set; } = [new string[3], new string[3], new string[3]];
 	public static PackedScene ChosenHazard { get; set; }
 	public static string ChosenAbility { get; set; }
 	public static int PlayerMoney { get; set; } = 1000;
