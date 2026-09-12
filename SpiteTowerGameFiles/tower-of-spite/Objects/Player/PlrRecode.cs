9af3f77ae39f11ab156c3db81604237f38982a3a
Squiddo <squiddytoonart@gmail.com>
2026-09-11 19:00:47 -0600

    Major Player Movement Added
    Main Changes to original:
    - Dash is no longer part of base code (Will be made modular)
    - Native Double Jump Support
    - Variable Jump Height
    - Fast Falling
    - Variable Acceleration & Deceleration
    
    Still Needs:
    - Coyote Time (WIP; Commented out)
    - Jump Buffering
    - Animation System

--- SpiteTowerGameFiles/tower-of-spite/Objects/Player/PlrRecode.cs
+++ SpiteTowerGameFiles/tower-of-spite/Objects/Player/PlrRecode.cs
@@ -2,8 +2,27 @@ using Godot;
 using System;
 public partial class PlrRecode : CharacterBody2D
 {
-	[Export] public float Speed = 300.0f;
-	[Export] public float JumpVelocity = -400.0f;
+	[Export] public float RunSpeed = 450.0f;
+	[Export(PropertyHint.Range, "0,1")] public float Deceleration = 0.3f;
+	[Export(PropertyHint.Range, "0,1")] public float Acceleration = 0.3f;
+	[Export] public float JumpHeight = -400.0f;
+	[Export(PropertyHint.Range, "0,1")] private float _jumpCutoff = 0.25f;
+	
+	[Export] public int MaxAirJumps = 0;
+	private int _jumpCount;
+
+	private bool _wasOnFloor;
+	private bool _justLeftFloor;
+
+	[Export] public float MaxStandardFallSpeed = 500f;
+	[Export(PropertyHint.Range, "1,5")] public float FastFallMultiplier = 1.5f;
+	private float _maxFallSpeed = 500f;
+	private bool _isFastFalling = false;
+	
+	// Ripped from original code
+	private float _dropThroughTimer = 0.0f;
+	private float _dropThroughTime = 0.15f;
+	private bool _droppingThrough = false;
 
 	public override void _PhysicsProcess(double delta)
 	{
@@ -15,25 +34,88 @@ public partial class PlrRecode : CharacterBody2D
 			velocity += GetGravity() * (float)delta;
 		}
 
-		// Handle Jump.
-		if (Input.IsActionJustPressed("jump") && IsOnFloor())
+		// Reset midair actions
+		if (IsOnFloor())
 		{
-			velocity.Y = JumpVelocity;
+			_isFastFalling = false;
+			_maxFallSpeed = MaxStandardFallSpeed;
+			_jumpCount = MaxAirJumps;
+		}
+		
+		// Handle Jump.
+		if (Input.IsActionJustPressed("jump") && (_jumpCount > 0 || IsOnFloor()))
+		{
+			if (!IsOnFloor())
+			{
+				_jumpCount--;
+			}
+			
+			velocity.Y = JumpHeight;
+		}
+		
+		// Variable Jump Height Cutoff
+		if (Input.IsActionJustReleased("jump") && velocity.Y < 0)
+		{
+			velocity.Y *= _jumpCutoff;
 		}
 
 		// Get the input direction and handle the movement/deceleration.
-		// As good practice, you should replace UI actions with custom gameplay actions.
 		Vector2 direction = Input.GetVector("move_left", "move_right", "move_up", "move_down");
+		
 		if (direction != Vector2.Zero)
 		{
-			velocity.X = direction.X * Speed;
+			velocity.X = Mathf.MoveToward(velocity.X, direction.X * RunSpeed, RunSpeed * Acceleration);
 		}
 		else
 		{
-			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
+			velocity.X = Mathf.MoveToward(Velocity.X, 0, RunSpeed * Deceleration);
 		}
 
+		// Fast Fall
+		if (velocity.Y > 0f && !IsOnFloor() && Input.IsActionJustPressed("move_down") && !_isFastFalling)
+		{
+			_isFastFalling = true;
+			_maxFallSpeed *= FastFallMultiplier;
+			velocity.Y = _maxFallSpeed;
+		}
+		
+		
+		// Ripped from original code
+		// Drop through platforms
+		if (Input.IsActionPressed("move_down"))
+		{
+			_droppingThrough = true;
+			_dropThroughTimer = _dropThroughTime;
+			
+			SetCollisionMaskValue(2, false);
+		}
+		
+		// Ripped from original code
+		// Handles falling through platforms
+		if (_droppingThrough)
+		{
+			_dropThroughTimer -= (float)delta;
+			
+			if (_dropThroughTimer <= 0)
+			{
+				_droppingThrough = false;
+				SetCollisionMaskValue(2, true);
+			}
+		}
+		
+		// Limiting Fall Speed
+		velocity.Y = Math.Min(velocity.Y, _maxFallSpeed);
+
+		// _wasOnFloor = IsOnFloor();
 		Velocity = velocity;
 		MoveAndSlide();
+		// _justLeftFloor = _wasOnFloor && !IsOnFloor() && velocity.Y >= 0f;
+		//
+		// if (_justLeftFloor)
+		// {
+		// 	
+		// }
 	}
 }
+
+
