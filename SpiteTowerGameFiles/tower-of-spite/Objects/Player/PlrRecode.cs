eca2d1c2dd3579e32d33ff5595f56868ab30b231
Squiddo <squiddytoonart@gmail.com>
2026-09-13 15:53:29 -0600

    Added Extra Comments; velocity is now public

--- SpiteTowerGameFiles/tower-of-spite/Objects/Player/PlrRecode.cs
+++ SpiteTowerGameFiles/tower-of-spite/Objects/Player/PlrRecode.cs
@@ -2,36 +2,44 @@ using Godot;
 using System;
 public partial class PlrRecode : CharacterBody2D
 {
+	// General Player Movement stats, hopefully can make upgrades change this
 	[Export] public float RunSpeed = 450.0f;
 	[Export(PropertyHint.Range, "0,1")] public float Deceleration = 0.3f;
 	[Export(PropertyHint.Range, "0,1")] public float Acceleration = 0.3f;
 	[Export] public float JumpHeight = -400.0f;
 	[Export(PropertyHint.Range, "0,1")] private float _jumpCutoff = 0.25f;
 	
+	// If this isnt obvious just by the name then you really need to get better at reading
+	// (Its the double jump stuff)
 	[Export] public int MaxAirJumps = 0;
 	private int _jumpCount;
-
-	private bool _wasOnFloor;
+	
+	// Coyote Time Shenanigans
+	private bool _wasOnFloor; 
 	private bool _justLeftFloor;
 
+	// Fall Speed Stuff
 	[Export] public float MaxStandardFallSpeed = 500f;
 	[Export(PropertyHint.Range, "1,5")] public float FastFallMultiplier = 1.5f;
 	private float _maxFallSpeed = 500f;
 	private bool _isFastFalling = false;
 	
 	// Ripped from original code
+	// Dropping through platform stats
 	private float _dropThroughTimer = 0.0f;
 	private float _dropThroughTime = 0.15f;
 	private bool _droppingThrough = false;
 
+	public Vector2 _velocity;
+
 	public override void _PhysicsProcess(double delta)
 	{
-		Vector2 velocity = Velocity;
+		_velocity = Velocity;
 
 		// Add the gravity.
 		if (!IsOnFloor())
 		{
-			velocity += GetGravity() * (float)delta;
+			_velocity += GetGravity() * (float)delta;
 		}
 
 		// Reset midair actions
@@ -50,13 +58,13 @@ public partial class PlrRecode : CharacterBody2D
 				_jumpCount--;
 			}
 			
-			velocity.Y = JumpHeight;
+			_velocity.Y = JumpHeight;
 		}
 		
 		// Variable Jump Height Cutoff
-		if (Input.IsActionJustReleased("jump") && velocity.Y < 0)
+		if (Input.IsActionJustReleased("jump") && _velocity.Y < 0)
 		{
-			velocity.Y *= _jumpCutoff;
+			_velocity.Y *= _jumpCutoff;
 		}
 
 		// Get the input direction and handle the movement/deceleration.
@@ -64,19 +72,19 @@ public partial class PlrRecode : CharacterBody2D
 		
 		if (direction != Vector2.Zero)
 		{
-			velocity.X = Mathf.MoveToward(velocity.X, direction.X * RunSpeed, RunSpeed * Acceleration);
+			_velocity.X = Mathf.MoveToward(_velocity.X, direction.X * RunSpeed, RunSpeed * Acceleration);
 		}
 		else
 		{
-			velocity.X = Mathf.MoveToward(Velocity.X, 0, RunSpeed * Deceleration);
+			_velocity.X = Mathf.MoveToward(Velocity.X, 0, RunSpeed * Deceleration);
 		}
 
 		// Fast Fall
-		if (velocity.Y > 0f && !IsOnFloor() && Input.IsActionJustPressed("move_down") && !_isFastFalling)
+		if (_velocity.Y > 0f && !IsOnFloor() && Input.IsActionJustPressed("move_down") && !_isFastFalling)
 		{
 			_isFastFalling = true;
 			_maxFallSpeed *= FastFallMultiplier;
-			velocity.Y = _maxFallSpeed;
+			_velocity.Y = _maxFallSpeed;
 		}
 		
 		
@@ -104,10 +112,10 @@ public partial class PlrRecode : CharacterBody2D
 		}
 		
 		// Limiting Fall Speed
-		velocity.Y = Math.Min(velocity.Y, _maxFallSpeed);
+		_velocity.Y = Math.Min(_velocity.Y, _maxFallSpeed);
 
 		// _wasOnFloor = IsOnFloor();
-		Velocity = velocity;
+		Velocity = _velocity;
 		MoveAndSlide();
 		// _justLeftFloor = _wasOnFloor && !IsOnFloor() && velocity.Y >= 0f;
 		//
